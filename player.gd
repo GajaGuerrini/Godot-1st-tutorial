060e461961089217d776587781e81340bc49e130
Gaja Guerrini <gaja.guerrini@gmail.com>
2024-01-02 23:55:30 +0100

    1st commit

---
+++ player.gd
@@ -0,0 +1,47 @@
+extends Area2D
+
+@export var SPEED = 400
+var velocity
+var screensize
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screensize = get_viewport_rect().size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):  #delta is the timestamp of the frame
+	# reset velocity
+	velocity = Vector2()
+	#check which keys have been pressed, ui_[nekineki] preset ( project setting -> input map )
+	if Input.is_action_pressed("ui_right"):
+		velocity.x +=1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -=1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y +=1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -=1
+	if velocity.length() > 0: # animation when we are moving
+		$AnimatedSprite2D.play()
+		velocity = velocity.normalized()*SPEED
+	else:
+		$AnimatedSprite2D.stop()
+	
+	position += velocity*delta
+	
+	# constrain the character in the screen
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+	
+	# Animation handeling
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "right"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+
+	if velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+		$AnimatedSprite2D.flip_h = false
+	
