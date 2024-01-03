9b95295ba38cd1a9d85cd4dd778cac7515c08358
Gaja Guerrini <gaja.guerrini@gmail.com>
2024-01-03 14:13:51 +0100

    adding mobs, spawning location and velocities

--- main.gd
+++ main.gd
@@ -3,17 +3,53 @@ extends Node2D
 var score
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready():  # choosing a random mob
 	randomize()
 
 
-func new_game():
+func new_game():  #setting new game
 	score = 0
-	$Player.start($StartPos4Player.position)
+	$Player.start($StartPos4Player.position) # repositioning player
 	$StartTimer.start()
 
 
 func game_over():
-	$ScoreTimer.stop()
-	$MobTimer.stop()
+	$ScoreTimer.stop() # our score timer stops
+	$MobTimer.stop()  # we dont spawn mobs anymore
+	
+
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+	
+
+
+func _on_score_timer_timeout():
+	score += 1
+
+
+func _on_mob_timer_timeout(): #spawning mobs timer
+	$MobPath/MobSpawnLocation.set_offset(randi())
+	var mob = Mob.instance()
+	add_child(mob)
+	var direction = $MobPath/MobSpawnLocation.rotation
+	mob.position = $MobPath/MobSpawnLocation.position
+	direction += randf_range(-PI/4, PI/4)
+	mob.rotation = direction
+	mob.set_linear_velocity(Vector2(randf_range(mob.MIN_SPEED, mob.MAX_SPEED), 0).rotated(direction))
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
